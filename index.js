d0e58aa9d6fe9356c2e4adf8732761303e28cea8
Vicki Hamm <vicki.cello@gmail.com>
2018-04-29 23:55:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -36,6 +36,8 @@ function checkCollision(rock) {
 
     if(((rockLeftEdge<dodgerLeftEdge)&&(rockRightEdge>dodgerLeftEdge)) || ((rockLeftEdge>dodgerLeftEdge)&&(rockRightEdge<dodgerRightEdge)) || ((rockLeftEdge<dodgerRightEdge)&&(rockRightEdge>dodgerRightEdge))) {
       return true;
+      
+      (((rockLeftEdge<dodgerRightEdge)&&(rockRightEdge>dodgerLeftEdge)) || ((rockLeftEdge>dodgerLeftEdge)&&(rockRightEdge<dodgerRightEdge)) || ((rockLeftEdge<dodgerRightEdge)&&(rockRightEdge>dodgerRightEdge))
     }
   }
 }
